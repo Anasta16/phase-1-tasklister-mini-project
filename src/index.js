2a9b71a93ca2ebe6504993d74876b1e57cb806c6
Anasta16 <97866619+Anasta16@users.noreply.github.com>
2023-01-12 22:45:42 -0500

    task lister main deliverables

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const form = document.getElementById('create-task-form')
+  form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    // console.log(form)
+
+    const newTaskInput = document.getElementById('new-task-description').value
+    const newTasks = document.getElementById('tasks')
+    const newLI = document.createElement('li')
+    newLI.textContent = newTaskInput
+    newTasks.appendChild(newLI)
+    console.log(newLI)
+    form.reset()
+
+    const deleteButton = document.createElement('button')
+    newLI.appendChild(deleteButton)
+    deleteButton.style.border = '10px solid red'
+    deleteButton.textContent = 'X'
+    deleteButton.addEventListener('click', (event) => {
+      console.log('deleteButton')
+      newLI.remove()
+    })
+  })
 });
